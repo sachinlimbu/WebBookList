cc20ffd76991d6f6b32553e5598509ddf92531d9
Sachin Limbu <sachinlimbu@outlook.com>
2019-06-01 08:32:55 +0100

    Add project files.

---
+++ WebBookList.Core/Book.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebBookList.Core
+{
+    public class Book
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [Display(Name = "Book Name")]
+        public string Name { get; set; }
+        public string ISBN { get; set; }
+        public string AuthorName { get; set; }
+    }
+}
---
+++ WebBookList.Data/WebBookListDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebBookList.Core;
+
+namespace WebBookList.Data
+{
+    public class WebBookListDbContext : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+
+        public WebBookListDbContext(DbContextOptions<WebBookListDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
